fd1e36c1c3507133d1f57732a4dbd904e2d82189
Revson <revson@gmail.com>
2019-08-18 10:57:21 -0300

    aula 32-G5

--- index.js
+++ index.js
@@ -8,3 +8,18 @@ var email = document.querySelector("#exampleInputEmail");
 var password = document.querySelector("#exampleInputPassword");
 var photo = document.querySelector("#exampleInputFile");
 var admin = document.querySelector("#exampleInputAdmin");
+
+// fields vai receber todos os elementos do formulario pelo nome
+var fields = document.querySelectorAll("#form-user-create [name]");
+
+// criar um foreach para ler e tratar todos so campos do formulario
+
+// tambem pode ser escrito como arrayfunction
+//fields.forEach((field, index)=>{});
+
+fields.forEach(function(field, index){
+
+    console.log(field.name);
+
+});
+
