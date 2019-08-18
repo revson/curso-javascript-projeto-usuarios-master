292ef3e5d727d79d91b184e30e1ae5b4e53c2c4a
Revson <revson@gmail.com>
2019-08-18 10:11:14 -0300

    aula 31-G4

---
+++ index.js
@@ -0,0 +1,10 @@
+var name = document.querySelector("#exampleInputName");
+//procure por todos os elementos com atributo name=gender e para filtrar mais uma vez
+//um pseudo filtro :checked
+var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail");
+var password = document.querySelector("#exampleInputPassword");
+var photo = document.querySelector("#exampleInputFile");
+var admin = document.querySelector("#exampleInputAdmin");
