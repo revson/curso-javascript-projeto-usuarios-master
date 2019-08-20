3f27537cf0f1c3222da1893f01387d559c916e47
Revson <revson@gmail.com>
2019-08-20 09:01:30 -0300

    aula 34-G7

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-var name = document.querySelector("#exampleInputName");
+
 //procure por todos os elementos com atributo name=gender e para filtrar mais uma vez
 //um pseudo filtro :checked
-var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
-var birth = document.querySelector("#exampleInputBirth");
-var country = document.querySelector("#exampleInputCountry");
-var email = document.querySelector("#exampleInputEmail");
-var password = document.querySelector("#exampleInputPassword");
-var photo = document.querySelector("#exampleInputFile");
-var admin = document.querySelector("#exampleInputAdmin");
+//var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
+
 
 // fields vai receber todos os elementos do formulario pelo nome
 var fields = document.querySelectorAll("#form-user-create [name]");
+var user = {};
+
 
 // criar um foreach para ler e tratar todos so campos do formulario
 
@@ -22,14 +19,15 @@ fields.forEach(function(field, index){
     if(field.name == "gender"){
        
         if(field.checked === true ){
-            console.log("sim", field);
+            user[field.name] = field.value;
         }
 
     }else{
-        console.log("nao");
+        user[field.name] = field.value;
     }
 
-    
 
 });
+
+console.log(user);
 
