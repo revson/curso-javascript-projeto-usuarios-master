7a80da140a57675ad8111cfaf5496aa15617cb17
Revson <revson@gmail.com>
2019-08-24 11:02:27 -0300

    aula 38-G11

--- index.js
+++ index.js
@@ -5,60 +5,9 @@
 
 
 // fields vai receber todos os elementos do formulario pelo nome
-var fields = document.querySelectorAll("#form-user-create [name]");
-var user = {};
-
-function addLine(dataUser){
-    
-    var tr = document.createElement("tr");
-
-    // usando a crase para fazer um template string
-    tr.innerHTML = `<tr>
-                    <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
-                    <td>${dataUser.name}</td>
-                    <td>${dataUser.email}</td>
-                    <td>${dataUser.admin}</td>
-                    <td>${dataUser.birth}</td>
-                    <td>
-                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                    </td>
-                </tr>`;
-    document.getElementById("table-users").appendChild(tr);
-
-    
-}
+// var fields = document.querySelectorAll("#form-user-create [name]");
+// tambem pode ser escrito como arrow function
+// fields.forEach((field, index)=>{});
 
 
-// criar um foreach para ler e tratar todos so campos do formulario
-
-// tambem pode ser escrito como arrayfunction
-//fields.forEach((field, index)=>{});
-
-
-
-
-// pegamos o evento submit do formulario
-document.getElementById("form-user-create").addEventListener("submit", function(event){
-
-    // cancela o comportamento padrao do form
-    event.preventDefault();
-
-    fields.forEach(function(field, index){
-
-        if(field.name == "gender"){
-           
-            if(field.checked === true ){
-                user[field.name] = field.value;
-            }
-    
-        }else{
-            user[field.name] = field.value;
-        }
-    
-    
-    });
-
-    addLine(user);
-
-});
+let userController =  new UserController("form-user-create", "table-users");
---
+++ models/controllers/UserControllers.js
@@ -0,0 +1,98 @@
+class UserController {
+
+    constructor(formId, tableId){
+
+        //inicia a variavel com os elementos do formulario
+        this.formEl = document.getElementById(formId);
+
+        //inicia a variavel da tabela que recebera os dados do usuario
+        this.tableEl = document.getElementById(tableId);
+
+        this.onSubmit();
+    }
+
+    /**
+     * Quando clicar no botao, o submit do formulario e montado aqui
+     */
+    onSubmit(){
+
+        
+        // pegamos o evento submit do formulario
+        // como ja pegamos o formulario no construtor
+        // aqui so passa a variavel formEl.
+        // Outra coisa aqui, vamos usar o arrow function dentro do metodo
+        // para evitar conflito com o this da classe que passaria a ser o this da function.        
+        // Nesta arrow function nao for colocado os () poi tem apenas um paramentro
+        this.formEl.addEventListener("submit", event => {
+
+            // cancela o comportamento padrao do form
+            event.preventDefault();
+
+            // pega os valores digitados no formulario e nao submete o formulario,
+            // apenas cria uma linha no grid com os dados do formulario
+            this.addLine( this.getValues() );
+
+            
+
+        });
+
+    }
+
+    /**
+     * Pega os valores dos campos do formulario
+     */
+    getValues(){
+        // declarando com let, para ficar somente dentro do escopo
+        let user = {};
+        
+        // vamos rodar o foreach nos elementos do formulario
+        this.formId.elements.forEach(function(field, index){
+
+            if(field.name == "gender"){
+               
+                if(field.checked === true ){
+                    user[field.name] = field.value;
+                }
+        
+            }else{
+                user[field.name] = field.value;
+            }
+        
+        
+        });
+        
+        // retornado direto a resposta da classe
+        return new User(
+            user.name, 
+            user.gender, 
+            user.birth, 
+            user.country, 
+            user.email, 
+            user.passwor, 
+            user.photo, 
+            user.admin);
+
+        
+    }
+
+    addLine(dataUser){
+                
+        // usando a crase para fazer um template string
+        this.tableEl.innerHTML = `
+                    <tr>
+                        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                        <td>${dataUser.name}</td>
+                        <td>${dataUser.email}</td>
+                        <td>${dataUser.admin}</td>
+                        <td>${dataUser.birth}</td>
+                        <td>
+                        <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                        <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                        </td>
+                    </tr>`;
+        
+    
+        
+    }
+    
+}
